28e11b9fb72e03bcb69b0d702700a445bfcdc30d
davidleite <davidluisleite22@gmail.com>
2026-09-10 17:24:41 -0300

    incluindo a main

---
+++ stardewManager/stardewManager.java
@@ -0,0 +1,17 @@
+public class stardewManager {
+    public static void main(String[] args) throws Exception {
+        
+        switch (menu) {
+            case 1:
+                
+                break;
+        
+            default:
+                break;
+        }
+        Animal vaca = new VacaMae("mugir","malhada", "Leite",101);
+        vaca.EmitirSom();
+        vaca.TipoDeRaca();
+        vaca.ColetarProduto();
+    }
+}
